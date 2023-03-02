291092602d9b3c48eb5601efec6834e68b1a66db
luan.tomiozzo <informatica19@martinello.com.br>
2023-03-02 11:04:43 -0400

    criando telas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,113 @@
-import logoDev from "../src/assets/logoDgProducoes.png";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+
+
+interface Mensagem {
+  id: number;
+  sender: string;
+  recipient: string;
+  content: string;
+  timestamp: []
+}
+
+
+
 function App() {
+
+  const [mensagem, setMensagem] = useState<Mensagem[]>([]);
+  const [remetente, setRemetente] = useState("");
+  const [destinatario, setDestinatario] = useState("");
+  const [message, setMessage] = useState("");
+
+  const carregar = async () => {
+    const response = await axios.get(`http://localhost:8080/api/chat/messages?sender=${remetente}&recipient=${destinatario}`)
+
+    setMensagem(response.data)
+
+    console.log(mensagem);
+
+  }
+
+  const sendeMensagem = async () => {
+
+    const response = await axios.post(`http://localhost:8080/api/chat/message?sender=${remetente}&recipient=${destinatario}&content=${message}`)
+
+    carregar()
+  };
+
+  const handleRemetente = (value: any) => {
+    setRemetente(value)
+  }
+
+  const handleDestinatario = (value: any) => {
+    setDestinatario(value)
+  }
+  const handleMessage = (value: any) => {
+    setMessage(value)
+  }
+
+  useEffect(() => {
+
+    carregar()
+
+  }, []);
+
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-purple-600 ">
-      <div className="flex flex-col justify-center items-center gap-4 bg-white p-4  rounded-lg shadow-2xl">
-        <h1 className="text-3xl font-bold text-purple-600 pb-10 ">
-          Template - TailwindCSS!
-        </h1>
-        <img
-          className="flex rounded-full h-52 animate-bounce shadow-2xl border border-purple-600"
-          src={logoDev}
-          alt=""
-        />
+    <div className="flex p-2 justify-center items-center w-screen h-screen bg-green-400 ">
+
+      <div className=" flex w-full items-center justify-center gap-4 flex-col w-1/2">
+
+
+        <div className="flex w-full justify-center gap-4 lg:flex flex-wrap items-center bg-white rounded p-2 ">
+
+          <input className="flex p-4  rounded-lg"
+            type="text" name="sender" id="sender"
+            placeholder="Remetente" onChange={(e) => handleRemetente(e.target.value)} />
+
+          <input className="flex p-4  rounded-lg"
+            type="text" name="recipient" id="recipient"
+            placeholder="Destinatario" onChange={(e) => handleDestinatario(e.target.value)} />
+
+        </div>
+
+        <div className=" flex  flex-col gap-2 bg-white p-2 rounded justify-end w-full " >
+          {mensagem.map((e) => (
+
+            <div key={e.id} className="flex justify-end flex-col  bg-green-100 p-2 rounded-lg">
+
+              <div className="flex  justify-end">
+                <span className="font-bold text-md">
+                  {e.sender}
+                </span>
+              </div>
+
+              <span className="flex  justify-end text-sm">
+                {e?.content}
+              </span>
+
+              <div className="flex  justify-end text-sm">
+                <span >
+                  Data
+                </span>
+              </div>
+
+            </div>
+          ))}
+        </div>
+
+        <div className="flex gap-8  items-center justify-between bg-gray-300 p-2 rounded">
+          <input className="flex p-4  rounded-lg"
+            type="text" name="message" id="message"
+            placeholder="Mensagem" onChange={(e) => handleMessage(e.target.value)} />
+
+          <button className="flex p-2 bg-green-300 rounded" onClick={sendeMensagem}>
+            Enviar
+          </button>
+        </div>
+
       </div>
-    </div>
+
+    </div >
   );
 }
 
